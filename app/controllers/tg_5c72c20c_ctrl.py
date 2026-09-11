88615fccf1bcedfcb6048f23bee73db98f9d8c02
Adison Jimenez <adisonzenemij@gmail.com>
2026-09-10 22:53:36 -0500

    feat: fd824721-34b3-487e-a3f9-1a4290e85bec

--- app/controllers/tg_5c72c20c_ctrl.py
+++ app/controllers/tg_5c72c20c_ctrl.py
@@ -7,7 +7,7 @@ from app.repositories.tg_5c72c20c_repo import Tg5c72c20cRepository
 from app.schemas.tg_5c72c20c_schema import CreateSchema, UpdateSchema, ResponseSchema, LoginSchema
 from app.security.security import create_access_token, verify_password
 from app.services.tg_5c72c20c_srvc import Tg5c72c20cService
-from app.models.entities_model import Tg2f997592Model, Tg5c72c20cModel, Tg8a26b478Model, Tg9a7bbe6fModel, Ms2e794a8fModel, Ms8b6bd18aModel
+from app.models.entities_model import Tg2f997592Model, Tg5c72c20cModel, Tg8a26b478Model, Tg8a2579bfModel, Tg9a7bbe6fModel, Ms2e794a8fModel, Ms8b6bd18aModel
 from app.dependencies.dependencies import get_current_user
 
 router = APIRouter()
@@ -35,9 +35,16 @@ def current_user_permissions(current_user: str = Depends(get_current_user), db: 
         .where(Tg8a26b478Model.tg_9a7bbe6f == user.tg_9a7bbe6f)
         .order_by(Ms8b6bd18aModel.fd_product, Ms2e794a8fModel.fd_name)
     ).all()
+    module_rows = db.execute(
+        select(Ms8b6bd18aModel.fd_product, Tg2f997592Model.fd_name)
+        .join(Tg8a2579bfModel, Tg8a2579bfModel.ms_8b6bd18a == Ms8b6bd18aModel.id_universal)
+        .join(Tg2f997592Model, Tg2f997592Model.id_universal == Tg8a2579bfModel.tg_2f997592)
+        .where(Tg8a2579bfModel.tg_9a7bbe6f == user.tg_9a7bbe6f)
+        .order_by(Ms8b6bd18aModel.fd_product)
+    ).all()
     return {"role": role.fd_name if role else None, "permissions": [
         {"module": module, "resource": resource, "access": access, "client": client}
         for module, resource, access, client in rows
-    ]}
+    ], "module_permissions": [{"module": module, "access": access} for module, access in module_rows]}
 
 router.include_router(create_crud_router("/users", ["Usuarios"], Tg5c72c20cService, CreateSchema, UpdateSchema, ResponseSchema))
